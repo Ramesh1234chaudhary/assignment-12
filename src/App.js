6a8a990861a44a4e9f64bec5e0f3fe0c83180159
ramesh <romanchaudhary01994@gmail.com>
2023-04-28 09:02:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Navbar from './Components/Navbar';
+import Product from './Components/Product';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Product/>
+      </div >
+      );
+      }
+    
+      export default App;
